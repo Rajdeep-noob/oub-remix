34de8bef936460169487cee5c525f407e56143a3
MD shakil <62477080+mdkaiom95@users.noreply.github.com>
2020-08-08 11:04:53 +0600

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -198,7 +198,7 @@ async def amireallyalive(alive):
              f"`Bot Version🤘: Remix {BOT_VER}` \n"
              f"==================================== \n"
              f"User 👨‍🚀: {DEFAULTUSER} \n"
-             f"Maintainer 🏄‍♂️: @heyworld \n"
+             f"Bot Owner 🏄‍♂️: @shakil29 \n"
              f"====================================\n")
     await bot.send_file(alive.chat_id, logo, caption=output)
     await alive.delete()
